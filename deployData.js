e6c761d4765618979cbfb425fcdb927efe4caf53
Viky_pebriyanto <Viky12345697@gmail.com>
2023-06-14 17:18:16 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Viky Pebriyanto'; // TODO: replace this
+
+const studentId = 'FE5599426'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://viky-fe5599426.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
